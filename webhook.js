d8ef855538eb476a564e09fbcd62b14a52a9f3ff
Turkyden <616287091@qq.com>
2019-08-03 20:21:43 +0800

    Fixed hook

--- webhook.js
+++ webhook.js
@@ -8,30 +8,13 @@ const secret = "wued@2019";
 //打开 8081 端口监听来自 Github 的 Push 推送
 http.createServer(function (req, res) {
   let chunks = '';
-  let state = 'Loading';
   req.on('data', function(chunk) {
 	  chunks += chunk.toString();
     const sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunks).digest('hex');
-    state = sig;
     if (req.headers['x-hub-signature'] == sig) {
-      state = 'Finished';
-      exec('sudo git pull');
-      //exec('cd ' + repo + ' && git pull');
+      exec('git pull');
     }
   });
 
-  res.end('Hello World Form DreamerHook State: ' + state);
+  res.end('Hello World Form DreamerHook');
 }).listen(8081);
-
-// const http = require('http');
-// const hostname = '0.0.0.0';
-// const port = 8080;
-// const server = http.createServer((req, res) => { 
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/plain');
-    // res.end('Hello World\n');
-// }); 
-
-// server.listen(port, hostname, () => { 
-    // console.log(`Server running at http://${hostname}:${port}/`);
-// });
